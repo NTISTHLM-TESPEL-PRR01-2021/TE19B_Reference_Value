ebff34bca1f303ff04e47f6ed684aae001bf26b6
Mikael <mikael.bergstrom@ga.ntig.se>
2021-03-11 10:54:56 +0100

    Initial commit

---
+++ Reference/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Reference
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int[] targetNumbers = {23, 56, 90, 100};
+
+      int[] targetNumbers2 = targetNumbers;
+
+
+      targetNumbers[0] = 255;
+
+      Console.WriteLine(targetNumbers2[0]);
+
+      // Console.WriteLine(targetNumbers[0]);
+
+      // DoStuff(targetNumbers);
+
+      // Console.WriteLine(targetNumbers[0]);
+      
+      
+      // int hp = 100;
+
+      // hp = DecreaseHp(hp);
+
+      // Console.WriteLine(hp);
+
+      Console.ReadLine();
+    }
+
+    public static void DoStuff(int[] numbers)
+    {
+      numbers[0] = 100;
+    }
+
+    public static int DecreaseHp(int h)
+    {
+      h -= 50;
+      return h;
+    }
+  }
+}
